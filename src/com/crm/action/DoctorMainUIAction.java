06a2c07080cd194e164ad269afc1e29a3a5ae35a
reeuse <wodyd0810@naver.com>
2025-10-14 16:04:39 +0900

    feat: add doctorMainUI

---
+++ src/com/crm/action/DoctorMainUIAction.java
@@ -0,0 +1,16 @@
+package com.crm.action;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+public class DoctorMainUIAction implements Action {
+
+	@Override
+	public String execute(HttpServletRequest request) throws ServletException, IOException {
+
+		return "doctorReserveList.html";
+	}
+
+}

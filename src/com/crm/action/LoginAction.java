a8b30d8e00768d87f4703f6ef7c0dc9a6a531514
reeuse <wodyd0810@naver.com>
2025-10-14 16:05:11 +0900

    feat: add Login

---
+++ src/com/crm/action/LoginAction.java
@@ -0,0 +1,35 @@
+package com.crm.action;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.crm.model.LoginDAO;
+
+public class LoginAction implements Action {
+
+	@Override
+	public String execute(HttpServletRequest request) throws ServletException, IOException {
+		String url = "login.html";
+		
+		String employeeId = request.getParameter("employeeId");
+		String pw = request.getParameter("pw");
+		String loginIp = request.getRemoteAddr();
+		String authority = new LoginDAO().login(employeeId, pw, loginIp);
+		
+		if(authority != null){
+			HttpSession session = request.getSession(true);
+			session.setAttribute("employeeId", employeeId);
+			session.setAttribute("sidebarInfo", new LoginDAO().getEmployeeInfo(employeeId));
+			if("관리자".equals(authority)){
+				url = "reserveList.html";
+			}
+			else url = "doctorReserveList.html";
+		}
+		return url;
+	}
+
+}

efa932d78d2c3e280c1f2292403dfa6b1fe8760b
KBH <galaxysgoo418@gmail.com>
2025-10-15 02:18:43 +0900

    refactor: file name update

--- src/com/crm/action/ActionFactory.java
+++ src/com/crm/action/ActionFactory.java
@@ -1,6 +1,9 @@
 package com.crm.action;
 
 import com.crm.action.patient.AddPatientAction;
+import com.crm.action.patient.AddPatientUIAction;
+import com.crm.action.patient.DeletePatientAction;
+import com.crm.action.patient.EditPatientAction;
 import com.crm.action.patient.GetDoctorListByDateAction;
 import com.crm.action.patient.GetPatientDetailAction;
 import com.crm.action.patient.GetPatientListAction;
@@ -15,8 +18,17 @@ public class ActionFactory {
 		Action a = null;
 		// 구현부
 		switch (cmd) {
+		case "deletePatientAction":
+			a = new DeletePatientAction();
+			break;
+		case "editPatientAction":
+			a = new EditPatientAction();
+			break;
 		case "addPatientAction":
 			a = new AddPatientAction();
+			break;
+		case "addPatientUI":
+			a = new AddPatientUIAction();
 			break;
 		case "getPatientDetailAction":
 			a = new GetPatientDetailAction();

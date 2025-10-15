8a73490b819d005c59a8afe90bd79405f230e0ee
kbh_kosta <galaxysgoo418@gmail.com>
2025-10-15 15:58:41 +0900

    refactor: return jsp name edit

--- src/com/crm/action/patient/GetDoctorListByDateAction.java
+++ src/com/crm/action/patient/GetDoctorListByDateAction.java
@@ -22,6 +22,6 @@ public class GetDoctorListByDateAction implements Action {
 
         req.setAttribute("result", result);
 
-        return "getMedicalRecord.jsp";
+        return "getDoctorListByDate.jsp";
     }
 }
